51b9088794827911dba55fec1ab8cf9773c8bfc3
Julia Aranovich <jkirnosova@mirantis.com>
2016-11-16 17:13:01 +0300

    Add title

--- src/app.js
+++ src/app.js
@@ -1,4 +1,18 @@
 import './styles/layout.less';
 import './styles/main.less';
 
-//document.getElementById('loading').outerHTML = '';
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends Component {
+  render() {
+    return (
+      <h1>{'Welcome to 100 blocks!'}</h1>
+    );
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('container')
+);
